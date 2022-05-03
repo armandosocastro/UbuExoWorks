4e4393720e13e405ba87342de1a566f93fc13625
asl1006 <asl1006@alu.ubu.es>
2022-05-03 17:08:17 +0200

    pruebas

--- backend/aplicacion/app.py
+++ backend/aplicacion/app.py
@@ -1,7 +1,10 @@
 # app.py
+import os
+
 from flask import Flask
 app = Flask(__name__)
 
+_port = os.environ.get('PORT', 5000)
 
 @app.route("/")
 def home():
@@ -10,4 +13,4 @@ def home():
 
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
-    app.run(threaded=True, port=80)
+    app.run(threaded=True, port=_port)
