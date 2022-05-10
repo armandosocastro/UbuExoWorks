710ad35da83c89c314bd405d2b36a1b4406427f2
asl1006 <asl1006@alu.ubu.es>
2022-05-10 17:17:10 +0200

    heroku

--- database/db.py
+++ database/db.py
@@ -5,9 +5,12 @@ from decouple import config
 
 def get_connection():
     try:
-        DATABASE_URL = os.environ.get('DATABASE_URL')
+        #DATABASE_URL = os.environ.get('DATABASE_URL')
+        DATABASE_URL = 'postgres://kbeqwsfgqwkxui:5f7c44f61a7f07badb4c9744daa1f5c5c3763b1eaf83dd5a277c5547bec1899e@ec2-3-231-82-226.compute-1.amazonaws.com:5432/dfdt60gdue5iln'
+        
         print(DATABASE_URL)
         #DATABASE_URL = 'postgresql://adminDB:3x0w0rks@localhost:5432/DBexoWorks'
+        
         return psycopg2.connect(
         #    host = config('PGSQL_HOST'),
         #   user = config('PGSQL_USER'),
