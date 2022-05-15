fa7255a8df1d9d23290dfef594ed79f29f4e297a
asl1006 <asl1006@alu.ubu.es>
2022-05-15 12:43:48 +0200

    heroku

--- run.py
+++ run.py
@@ -3,7 +3,7 @@ import os
 
 app = create_app()
 
-#if __name__ == '__main__':
+if __name__ == '__main__':
 
-_port = os.environ.get('PORT', 5000)
-app.run()
+    _port = os.environ.get('PORT', 5000)
+    app.run('0.0.0.0', port=_port)
