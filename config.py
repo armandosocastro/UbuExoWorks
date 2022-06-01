e8a9908e781e5e1d07abf52d4b0c8d6386280d85
asl1006 <asl1006@alu.ubu.es>
2022-06-01 16:56:07 +0200

    Correccion seguridad

--- config.py
+++ config.py
@@ -6,7 +6,6 @@ from flask import Config
 class Config:
     #SECRET_KEY=config('SECRET_KEY')
     SECRET_KEY='password'
-    #SQLALCHEMY_DATABASE_URI = 'postgresql://adminDB:3x0w0rks@localhost:5432/DBexoWorks'
     #URI a la BD alojada en Heroku
     SQLALCHEMY_DATABASE_URI = 'postgresql://nmgntihzbdruoo:f6217fe082658c015953604284046c8743268291d990cc2d3c57cae72929278c@ec2-3-211-221-185.compute-1.amazonaws.com:5432/d4i2m2e3fn1svh'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
