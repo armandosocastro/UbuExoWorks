a1da26caf172c71fe009108552185831885bc16d
asl1006 <asl1006@alu.ubu.es>
2022-09-22 08:58:24 +0200

    #35

--- run.py
+++ run.py
@@ -11,5 +11,5 @@ if __name__ == '__main__':
 
     _port = os.environ.get('PORT', 5000)
     context = ('./cert/selfsigned.crt', './cert/selfsigned.key')
-    app.run('0.0.0.0', port=_port, debug=True)
-    #app.run('0.0.0.0', port=_port, ssl_context=context)
+    #app.run('0.0.0.0', port=_port, debug=True)
+    app.run('0.0.0.0', port=_port, ssl_context=context)
